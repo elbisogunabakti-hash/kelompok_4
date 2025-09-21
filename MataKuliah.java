65a7f45862517a6684be3294891a9cb0cdbfcd7d
elbisogunabakti <elbisogunabakti1@gmail.com>
2025-09-21 14:10:24 +0700

    Code MataKuliah.java

---
+++ MataKuliah.java
@@ -0,0 +1,14 @@
+public class MataKuliah{
+    public String kodeMataKuliah;
+    public String namaMataKuliah;
+
+    public void menampilkanJumlahSks(){
+        System.out.println("1 Sks Teori, 2 Sks Praktikum");
+
+    }
+
+    public String menampilkanDeskripsi(){
+        return "Mata Kuliah yang mempelajari paradigma pemrograman berorientasi objek";
+    }
+
+}
